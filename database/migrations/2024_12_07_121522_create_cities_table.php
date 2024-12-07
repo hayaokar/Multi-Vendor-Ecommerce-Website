5b219328b027ff1860d7eacbb2ac9b0fc3b7ad61
hayaokar <princesshion@hotmail.com>
2024-12-07 19:42:36 +0400

    migration

---
+++ database/migrations/2024_12_07_121522_create_cities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->integer('region_id');
+            $table->integer('country_id');
+            $table->decimal('latitude');
+            $table->decimal('longitude');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
