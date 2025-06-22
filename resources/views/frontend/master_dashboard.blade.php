48d9303b951e5f90e3d1f05d2dd43945d750330f
hayaokar <princesshion@hotmail.com>
2025-06-22 17:50:24 +0400

    user search

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -82,7 +82,7 @@
 <!-- Template  JS -->
 <script src="{{ asset('frontend/assets/js/main.js?v=5.3') }}"></script>
 <script src="{{ asset('frontend/assets/js/shop.js?v=5.3') }}"></script>
-<script src="{{ asset('frontend/assets/js/script.js') }}"></script>
+{{--<script src="{{ asset('frontend/assets/js/script.js') }}"></script>--}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
@@ -780,6 +780,53 @@
             }
         })
     }
+
+    $("#search").on('keyup',function (){
+        if(($(this).val()).length>2){
+            let serach = $(this).val()
+            $.ajax({
+                type:"POST",
+                datatype: "JSON",
+                url:"/search-ajax",
+                data:{
+                    "search":serach
+                },
+                success: function (data){
+                    console.log(data)
+
+                    if(data.length){
+                        html = ''
+                        $.each(data,function (key,value){
+                            html +=  '<a href="'+baseUrl+'/product/details/'+value.id+'/'+value.product_slug+'">'+
+                                '<div class="list border-bottom">'+
+                                '<div class="row">'+
+
+                                '<div class="col-md-2">'+
+
+                                '<img src="'+baseUrl+'/'+value.product_thambnail+'" style="width:40px; height:40px">'+
+                                '</div>'+
+                                '<div class="col-md-10">'+
+                                ' <div class="d-flex flex-column ml-5">'+
+                                ' <span>'+value.product_name+'</span> <small>$'+value.selling_price+'</small>'+
+
+                                '</div>'+
+                                '</div>'+
+
+                                ' </div>'+
+                                '</div>'+
+                                '</a>'
+                        });
+                    }else{
+                        html = '<span class="text-center text-muted">No products found</span>'
+                    }
+                    $('#result-info').html(html)
+
+                }
+
+            })
+        }
+    });
+
 </script>
 
 
